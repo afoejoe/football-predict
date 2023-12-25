9c9e3c2afe88f7f1fd018356f79ef78dc149c7d8
Afoejoe <afoejoe@gmail.com>
2023-12-25 21:15:23 +0300

    ci

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,6 +2,14 @@ module.exports = {
     apps: [{
       name: "predict.dev",
       "script": "make run",
+      "exec_interpreter": "none",
+      "exec_mode": "",
+      "env": {
+          "NODE_ENV": "production",
+          "PORT": 4000,
+          "HOST": "127.0.0.1",
+          "ORIGIN": "http://api.naijaanswers.com"
+      }
     }],
     deploy: {
       // "dev" is the environment name
@@ -11,7 +19,7 @@ module.exports = {
         // SSH user
         user: "deploy",
         // SSH host
-        host: ["156.227.0.205"],
+        host: "156.227.0.205",
         // SSH options with no command-line flag, see 'man ssh'
         // can be either a single string or an array of strings
         ssh_options: "StrictHostKeyChecking=no",
@@ -20,7 +28,7 @@ module.exports = {
         // GIT remote
         repo: "git@github.com:afoejoe/football-predict.git",
         // path in the server
-        path: "~/web/predict-test/apps/development/",
+        path: "/home/deploy/web/predict-test/apps/development",
         // Pre-setup command or path to a script on your local machine
         'pre-setup': "ls -la && echo 'pre set up'",
         // Post-setup commands or path to a script on the host machine
@@ -29,12 +37,12 @@ module.exports = {
         // pre-deploy action
         'pre-deploy-local': "echo 'This is a local executed command'",
         // post-deploy action
-        "post-deploy": "touch .envrc && > .envrc && echo DB_DSN=$DEV_DB_DSN >> .envrc && echo HTTP_PORT=$DEV_HTTP_PORT >> .envrc && echo basic-auth-hashed-password=$basic-auth-hashed-password >> .envrc  && echo notifications-email=$notifications-email >> .envrc && make build && pm2 reload ecosystem.config.js --env dev && pm2 save",
+        "post-deploy": "touch .envrc && > .envrc && echo DB_DSN=$DEV_DB_DSN >> .envrc && echo HTTP_PORT=$DEV_HTTP_PORT >> .envrc && echo BASIC_AUTH_HASHED_PASSWORD=$BASIC_AUTH_HASHED_PASSWORD >> .envrc  && echo NOTIFICATIONS_EMAIL=$NOTIFICATIONS_EMAIL >> .envrc && make build && pm2 reload ecosystem.config.js --env dev && pm2 save",
         "env": {
             "DEV_DB_DSN": process.env.DEV_DB_DSN,
             "DEV_HTTP_PORT": process.env.DEV_HTTP_PORT,
-            "basic-auth-hashed-password": process.env.basic-auth-hashed-password,
-            "notifications-email": process.env.notifications-email,
+            "BASIC_AUTH_HASHED_PASSWORD": process.env.BASIC_AUTH_HASHED_PASSWORD,
+            "NOTIFICATIONS_EMAIL": process.env.NOTIFICATIONS_EMAIL,
         }
       },
     }
